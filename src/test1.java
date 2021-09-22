e48db626daddb1d45540b1b948588c60ec5bebd3
Manish Fanibhare <fanibharemanish@gmail.com>
2021-09-22 11:33:33 +0530

    Test1 file will commit

---
+++ src/test1.java
@@ -0,0 +1,13 @@
+
+public class test1 {
+
+	public static void main(String[] args) {
+		
+		int a = 10, b = 2, c;
+
+		c = a + b;
+		
+		System.out.println("Addition of two a and b = " + c);
+	}
+
+}
